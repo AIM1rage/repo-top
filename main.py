73301b5a983d33b0f6c28f7bdd676a2337084e9a
AIM1rage <michael050100@gmail.com>
2023-10-25 19:11:08 +0500

    Почти рабочий код

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import asyncio
 import json
 import httpx
+from collections import Counter
 
 
 class AsyncApiGithubClient:
     def __init__(self, token):
         self.client = httpx.AsyncClient()
         self.headers = {"Accept": "application/vnd.github.v3+json",
-                        "Authorization": f'Bearer {token}'}
+                        "Authorization": f'Bearer {token}',
+                        "X-GitHub-Api-Version": "2022-11-28"}
         self.limit = None
         self.remaining = None
         self.used = None
@@ -26,21 +28,37 @@ class AsyncApiGithubClient:
         return response
 
     async def fetch_top_committers(self, organization):
-        ...
+        repos = await self._fetch_repos_(organization)
+
+        tasks = []
+        for repo_name in (r['full_name'] for r in repos):
+            task = self._fetch_commits_(repo_name)
+            tasks.append(task)
+        repos_commits = await asyncio.gather(*tasks)
+
+        authors = Counter()
+        for commits in repos_commits:
+            for author in (c['commit']['author']['name'] for c in commits if
+                           not c['commit']['message'].startswith(
+                               'Merge pull request #')):
+                authors[author] += 1
+        return authors
+
+    async def _fetch_data_from_pages_(self, url):
+        response = await self._get_request_(url, {"per_page": 100})
+        data = json.loads(response.content)
+        while 'next' in response.links:
+            response = await self._get_request_(response.links['next']['url'])
+            data.extend(json.loads(response.content))
+        return data
 
     async def _fetch_repos_(self, organization):
         url = f'https://api.github.com/orgs/{organization}/repos'
-        response = await self._get_request_(url, {"per_page": 100})
-        repos = json.loads(response.content)
-        while 'next' in response.links:
-            response = await self._get_request_(response.links['next']['url'])
-            repos.extend(json.loads(response.content))
-        return repos
+        return await self._fetch_data_from_pages_(url)
 
     async def _fetch_commits_(self, repository):
-        url = f'https://api.github.com/repos/skbkontur/{repository}'
-        response = await self._get_request_(url)
-        return response
+        url = f'https://api.github.com/repos/{repository}/commits'
+        return await self._fetch_data_from_pages_(url)
 
 
 async def main():
@@ -49,16 +67,17 @@ async def main():
     try:
         client = AsyncApiGithubClient(token)
 
-        task = asyncio.create_task(client._fetch_commits_('markdown'))
+        task = asyncio.create_task(
+            client.fetch_top_committers(organization))
 
         data = await task
-        print(data)
+        print(client.used)
+        print(data.total())
+        for author, contributions in data.most_common(10000000):
+            print(f'{author}: {contributions}')
     except httpx.HTTPError as err:
         print(f"Ошибка при выполнении запроса: {err}")
-    except Exception as err:
-        print(f"Произошла ошибка: {err}")
 
 
-# Запускаем асинхронный цикл событий и выполняем программу
 if __name__ == "__main__":
     asyncio.run(main())
